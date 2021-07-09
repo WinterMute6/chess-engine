ccaa2b5fae7eedf50c6bb0c4a5ecc5d3397ba6be
Aslan Boybekov <aslan.boybekov@gmail.com>
2021-07-09 15:54:33 -0400

    we added more features to the board class

--- chess-engine/Program.cs
+++ chess-engine/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace chess_engine
 {
@@ -6,22 +8,60 @@ namespace chess_engine
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            var board = new Board();
+            SetupBoard(board);
+            var cell = board.GetCell('h', 8);
+
+
+
+            /*List<Move> GetAvailableMoves(Cell initial)
+            { 
+                var availableMoves = new List<Move>();
+
+                return availableMoves;
+
+            }*/
         }
 
-     
-            
-
-            
-    }
-
-    class Board
-    {
-        public void GetCell()
+        public static void SetupBoard(Board board)
         {
+            foreach (var col in "abcdefgh")
+            {
+                board.GetCell(col, 2).Piece = new Piece(Color.White, Figure.Pawn);
+                board.GetCell(col, 7).Piece = new Piece(Color.Black, Figure.Pawn);
+            }
 
+            board.GetCell('a', 1).Piece = new Piece(Color.White, Figure.Rook);
+            board.GetCell('h', 1).Piece = new Piece(Color.White, Figure.Rook);
+            board.GetCell('a', 8).Piece = new Piece(Color.Black, Figure.Rook);
+            board.GetCell('h', 8).Piece = new Piece(Color.Black, Figure.Rook);
+
+            board.GetCell('b', 1).Piece = new Piece(Color.White, Figure.Knight);
+            board.GetCell('g', 1).Piece = new Piece(Color.White, Figure.Knight);
+            board.GetCell('b', 8).Piece = new Piece(Color.Black, Figure.Knight);
+            board.GetCell('g', 8).Piece = new Piece(Color.Black, Figure.Knight);
+
+            board.GetCell('c', 1).Piece = new Piece(Color.White, Figure.Bishop);
+            board.GetCell('f', 1).Piece = new Piece(Color.White, Figure.Bishop);
+            board.GetCell('c', 8).Piece = new Piece(Color.Black, Figure.Bishop);
+            board.GetCell('f', 8).Piece = new Piece(Color.Black, Figure.Bishop);
+
+            board.GetCell('d', 1).Piece = new Piece(Color.White, Figure.Queen);
+            board.GetCell('d', 8).Piece = new Piece(Color.Black, Figure.Queen);
+            board.GetCell('e', 1).Piece = new Piece(Color.White, Figure.King);
+            board.GetCell('e', 8).Piece = new Piece(Color.Black, Figure.King);
         }
 
 
     }
+
+
+
+    class Move
+    {
+        public Cell From { get; set; }
+        public Cell To { get; set; }
+    }
+}
 
