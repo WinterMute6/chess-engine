9138460c2b102f46e57cd3ca8de61353b2a96449
Aslan Boybekov <aslan.boybekov@gmail.com>
2021-07-19 13:24:50 -0400

    Refactored all the material inside the Pawn sub-class to the Piece base class

--- chess-engine/Pawn.cs
+++ chess-engine/Pawn.cs
@@ -12,63 +12,22 @@ namespace chess_engine
 
         override public List<Move> GetAvailableMoves()
         {
-            // Refactored Material To Be set in Base Class
-
-            var firstMove = new List<int>();
-            bool leftEdge;
-            bool rightEdge;
-
-
-            int moveUp;
-            int moveDown;
-            int moveUpLeft;
-            int moveUpRight;
-            int moveDownLeft;
-            int moveDownRight;
-            
-            if (this.Color == Color.White)
-            {
-                firstMove = new List<int>() {8,9,10,11,12,13,14,15}; 
-                rightEdge = this.Cell.IsRightEdge;
-                leftEdge = this.Cell.IsLeftEdge;
-                moveUp = 8;
-                moveDown = -8;
-                moveUpLeft = 7;
-                moveUpRight = 9;
-                moveDownLeft = -9;
-                moveDownRight = -7;
-            }
-            else
-            {
-                firstMove = new List<int>() {48,49,50,51,52,53,54,55};
-                leftEdge = this.Cell.IsRightEdge;
-                rightEdge = this.Cell.IsLeftEdge;
-                moveUp = -8;
-                moveDown = 8;
-                moveUpLeft = -7;
-                moveUpRight = -9;
-                moveDownLeft = 9;
-                moveDownRight = 7;
-            }
-
-            //Pawn Move Logic
-
             var board = this.Cell.Board;
             var availableMoves = new List<Move>();
             
-            if (!leftEdge && board.Cells[this.Cell.Number + moveUpLeft].IsOccupied && IsOppositeColor(board.Cells[this.Cell.Number + moveUpLeft].Piece.Color))
-                availableMoves.Add(new Move { From = this.Cell.Number, To = this.Cell.Number + moveUpLeft });
+            if (!LeftEdge() && board.Cells[MoveUpLeft(1)].IsOccupied && IsOppositeColor(board.Cells[MoveUpLeft(1)].Piece.Color))
+                availableMoves.Add(new Move { From = this.Cell.Number, To = MoveUpLeft(1) });
+                
+            if (!RightEdge() && board.Cells[MoveUpRight(1)].IsOccupied && IsOppositeColor(board.Cells[MoveUpRight(1)].Piece.Color))
+                availableMoves.Add(new Move { From = this.Cell.Number, To = MoveUpRight(1) });
 
-            if (!rightEdge && board.Cells[this.Cell.Number + moveUpRight].IsOccupied && IsOppositeColor(board.Cells[this.Cell.Number + moveUpRight].Piece.Color))
-                availableMoves.Add(new Move { From = this.Cell.Number, To = this.Cell.Number + moveUpRight });
-
-            if (board.Cells[this.Cell.Number + moveUp].IsEmpty)
+            if (board.Cells[MoveUp(1)].IsEmpty)
             {
-                availableMoves.Add(new Move { From = this.Cell.Number, To = this.Cell.Number + moveUp });
-                if (board.Cells[this.Cell.Number + (moveUp * 2)].IsEmpty)
+                availableMoves.Add(new Move { From = this.Cell.Number, To = MoveUp(1) });
+                if (board.Cells[MoveUp(2)].IsEmpty)
                 {
-                    if (firstMove.Contains(this.Cell.Number))
-                        availableMoves.Add(new Move { From = this.Cell.Number, To = this.Cell.Number + (moveUp * 2) });
+                    if (PawnFirstMove().Contains(this.Cell.Number))
+                        availableMoves.Add(new Move { From = this.Cell.Number, To = MoveUp(2) });
                 }
             }
             return availableMoves;
--- chess-engine/Piece.cs
+++ chess-engine/Piece.cs
@@ -6,8 +6,48 @@ namespace chess_engine
     public class Piece
     {
         private Color _color;
-
-      
+        public List<int> PawnFirstMove()
+        {
+            if (this.Color == Color.White)
+                return new List<int>() { 8, 9, 10, 11, 12, 13, 14, 15 };
+            else
+                return new List<int>() { 48, 49, 50, 51, 52, 53, 54, 55 };
+        }
+        public bool RightEdge()
+        {
+            if (this.Color == Color.White)
+                return this.Cell.IsRightEdge;
+            else
+                return this.Cell.IsLeftEdge;
+        }
+        public bool LeftEdge()
+        {
+            if (this.Color == Color.White)
+                return this.Cell.IsLeftEdge;
+            else
+                return this.Cell.IsRightEdge;
+        }
+        public int MoveUp(int iterant)
+        {
+            if (this.Color == Color.White)
+                return this.Cell.Number + (8 * iterant);
+            else
+                return this.Cell.Number - (8 * iterant);
+        }
+        public int MoveUpLeft(int iterant)
+        {
+            if (this.Color == Color.White)
+                return this.Cell.Number + (7 * iterant);
+            else
+                return this.Cell.Number - (7 * iterant);
+        }
+        public int MoveUpRight(int iterant)
+        {
+            if (this.Color == Color.White)
+                return this.Cell.Number + (9 * iterant);
+            else
+                return this.Cell.Number - (9 * iterant);
+        }
         public bool IsOppositeColor(Color color)
         {
             if (color == this.Color)
