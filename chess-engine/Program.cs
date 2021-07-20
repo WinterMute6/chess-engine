71d9818e854032d9cde73bc2754f8510406e2543
Aslan Boybekov <aslan.boybekov@gmail.com>
2021-07-19 23:19:05 -0400

    More work done to Rook GetAvailableMoves()

--- chess-engine/Program.cs
+++ chess-engine/Program.cs
@@ -46,21 +46,57 @@ namespace chess_engine
 
             for (int i = 1; i < 100; i++)
             {
-                do
+                if (board.Cells[MoveUp(i)].IsOccupied && IsOppositeColor(board.Cells[MoveUp(i)].Piece.Color))
                 {
-                    if (board.Cells[MoveUp(i)].IsOccupied && IsOppositeColor(board.Cells[MoveUp(i)].Piece.Color))
-                    {
-                        availableMoves.Add(new Move { From = this.Cell.Number, To = MoveUp(i) });
-                        break;
-                    }
-                    if (board.Cells[MoveUp(i)].IsOccupied)
-                    {
-                        break;
-                    }
                     availableMoves.Add(new Move { From = this.Cell.Number, To = MoveUp(i) });
+                    break;
                 }
-                while (board.Cells[MoveUp(i)].IsEmpty);
+                if (board.Cells[MoveUp(i)].IsOccupied)
+                {
+                    break;
+                }
+                availableMoves.Add(new Move { From = this.Cell.Number, To = MoveUp(i) });
             }
+            for (int i = 1; i < 100; i++)
+            {
+                if (board.Cells[MoveDown(i)].IsOccupied && IsOppositeColor(board.Cells[MoveDown(i)].Piece.Color))
+                {
+                    availableMoves.Add(new Move { From = this.Cell.Number, To = MoveDown(i) });
+                    break;
+                }
+                if (board.Cells[MoveDown(i)].IsOccupied)
+                {
+                    break;
+                }
+                availableMoves.Add(new Move { From = this.Cell.Number, To = MoveDown(i) });
+            }
+            for (int i = 1; i < 100; i++)
+            {
+                if (board.Cells[MoveRight(i)].IsOccupied && IsOppositeColor(board.Cells[MoveRight(i)].Piece.Color))
+                {
+                    availableMoves.Add(new Move { From = this.Cell.Number, To = MoveRight(i) });
+                    break;
+                }
+                if (board.Cells[MoveRight(i)].IsOccupied)
+                {
+                    break;
+                }
+                availableMoves.Add(new Move { From = this.Cell.Number, To = MoveRight(i) });
+            }
+            for (int i = 1; i < 100; i++)
+            {
+                if (board.Cells[MoveLeft(i)].IsOccupied && IsOppositeColor(board.Cells[MoveLeft(i)].Piece.Color))
+                {
+                    availableMoves.Add(new Move { From = this.Cell.Number, To = MoveLeft(i) });
+                    break;
+                }
+                if (board.Cells[MoveLeft(i)].IsOccupied)
+                {
+                    break;
+                }
+                availableMoves.Add(new Move { From = this.Cell.Number, To = MoveLeft(i) });
+            }
+
             return availableMoves;
         }
     }
